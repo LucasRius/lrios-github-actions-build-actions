f69b88d8c9178fd25f2a69dd706a8d3fcc6c1af5
Lucas Rios <lucasrius@gmail.com>
2025-11-14 17:22:26 -0300

    JS Complete -- quebra

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-function run(){
-     core.notice('Hello JS Action jovem')
+function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+    const webSiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('site-url', webSiteUrl)
 }
 
 run();
